28751185d1a1fc04ea103b413d11b3d006960bf7
Stephen Dupré <stephendupre64@gmail.com>
2017-10-26 15:29:35 +0200

    mise en place du layout et intégration du bootstrap

---
+++ public/index.php
@@ -0,0 +1,2 @@
+<?php
+require "./../views/layout.php";
---
+++ views/layout.php
@@ -0,0 +1,69 @@
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="réseau social pour les développeurs">
+    <meta name="author" content="dév'art">
+
+    <title>Social Codeur</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../public/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../public/assets/css/main.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+
+<body>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="#">Social Codeur</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Connexion </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Inscription </a>
+            </li>
+
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+
+<main role="main" class="container">
+
+    <div class="jumbotron">
+        <h1>Social Network ?</h1>
+        <p>
+            Social Network est le réseau social des développeurs ⌨. <br/>
+            Et qui dit développeurs, dit égalemet code source! ✍ <br/>
+            Grâce à cette plateforme, vous avez la possibilité de tisser des liens d'amitiés avec d'autres développeurs, recevoir de l'aide si vous rencontrez des problèmes sur l'un de vos projets et bien plus encore! <br/>
+            Alors n'hésitez plus et <a href="inscription.php">rejoingnez dès maintenant la communauté les amis! ☺ </a><br/></p>
+        <a href="inscription.php" class="btn btn-primary btn-lg">Je m'inscris</a>
+    </div>
+
+</main><!-- /.container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="../public/bootstrap/assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="../public/bootstrap/assets/js/vendor/popper.min.js"></script>
+<script src="../public/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
